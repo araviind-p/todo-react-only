dc13fa0c8327860d9a1634a564a3367b9354f837
araviind-p <aravindparavind10@gmail.com>
2023-09-08 22:08:20 +0530

    Updates

---
+++ precache-manifest.dc9c752ec1b3c338d2f4d7418c089e4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7e85d0d8cb184b3cd4d7d07226ca52",
+    "url": "/todoReact/index.html"
+  },
+  {
+    "revision": "6a0ee2bc8861b4257f7c",
+    "url": "/todoReact/static/js/2.2fa6d158.chunk.js"
+  },
+  {
+    "revision": "b324397517bf30c615a1",
+    "url": "/todoReact/static/js/main.67802ccb.chunk.js"
+  },
+  {
+    "revision": "83c906bb54d94a4013d4",
+    "url": "/todoReact/static/js/runtime-main.53827e55.js"
+  }
+]);
